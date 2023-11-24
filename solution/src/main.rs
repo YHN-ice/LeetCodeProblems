6e8e7e32eb733a6d5334330f723fc52e17f5cf62
Haoning <HaoningYe@outlook.com>
2023-11-24 17:28:20 -0500

    2023.11.24 Medium#2906 :vector:matrix:prefixSum: Although this code is correct, but it used closure with side-effect (line37), which is evil. In fact, when you collapse line39-40 into lien42, you will get unexpected behaviour, because iterator is lazy, the two `rev` calls would cancel as if no `rev` has been called, i.e., `<iter>.rev().map().rev()` behaves like  `<iter>.map()`.

--- solution/src/main.rs
+++ solution/src/main.rs
@@ -2,50 +2,49 @@ mod test;
 
 pub struct Solution;
 
-// Definition for a binary tree node.
-#[derive(Debug, PartialEq, Eq)]
-pub struct TreeNode {
-    pub val: i32,
-    pub left: Option<Rc<RefCell<TreeNode>>>,
-    pub right: Option<Rc<RefCell<TreeNode>>>,
-}
+trait Linear<T> {
+    fn lget(&self, i:usize) -> &T ;
 
-impl TreeNode {
-    #[inline]
-    pub fn new(val: i32) -> Self {
-        TreeNode {
-            val,
-            left: None,
-            right: None,
-        }
+    fn lset(&mut self, i:usize)-> &mut T ;
+}
+impl Linear<i32> for Vec<Vec<i32>> {
+    fn lget(&self, i:usize) -> &i32 {
+        let n = self[0].len();
+        &self[i/n][i%n]
+    }
+    fn lset(&mut self, i:usize)-> &mut i32 {
+        let n = self[0].len();
+        &mut self[i/n][i%n]
     }
 }
 
-use std::rc::Rc;
-use std::cell::RefCell;
-
-type T = Option<Rc<RefCell<TreeNode>>>;
-
-fn cnt(n:i32)-> i32 {
-    (1..=9).map(|x| if (n>>x)&1==1 {1} else {0}).sum()
-}
-fn dfs(root: T, acc: i32) -> i32 {
-    match root {
-        Some(rf) => {
-            let cur = acc ^ (1 << rf.borrow().val);
-            if rf.borrow().left.is_none() && rf.borrow().right.is_none() && cnt(cur) <= 1 {
-                1
-            } else {
-                dfs(rf.borrow().left.clone(),cur) +dfs(rf.borrow().right.clone(), cur)
-            }
-        }
-        None => 0
-    }
-}
+const  MOD:i32 = 12345;
 
 impl Solution {
-    pub fn pseudo_palindromic_paths(root: Option<Rc<RefCell<TreeNode>>>) -> i32 {
-        dfs(root, 0)
+    pub fn construct_product_matrix(grid: Vec<Vec<i32>>) -> Vec<Vec<i32>> {
+        let fold_n = |data,  n:usize| -> Vec<Vec<i32>> {
+            let mut ret = vec![];
+            let mut buf = vec![];
+            for d in data {
+                if buf.len()==n {ret.push(buf.clone()); buf.clear();}
+                buf.push(d);
+            }
+            if buf.len()>0 {ret.push(buf);}
+            ret
+        };
+        let nm = grid.len()*grid[0].len();
+        let (mut acc, mut racc) = (1, 1);
+        let mut pref = (0..nm).rev().map(|x| {acc = acc*(if x== nm-1 {1} else {grid.lget(x+1)%MOD})%MOD; acc})
+            .enumerate()
+            .collect::<Vec<_>>();
+        pref.reverse();
+        fold_n(
+         pref.into_iter().map(|(i, var)| {
+                let ret = (var * racc) %MOD;
+                racc = racc * (grid.lget(nm-1-i) %MOD) %MOD;
+
+                ret
+            }), grid[0].len())
     }
 }
 
--- solution/src/test.rs
+++ solution/src/test.rs
@@ -1,88 +1,20 @@
 #[cfg(test)]
 mod test {
-    use std::collections::VecDeque;
-    fn to_vec(tree:Option<Rc<RefCell<TreeNode>>>) -> Vec<Option<i32>> {
-        if tree.is_none() {return vec![]}
-        let mut ret = vec![];
-        let mut q = VecDeque::new();
-        q.push_back(tree);
-        while !q.is_empty() {
-            let cur = q.pop_front().unwrap();
-            match cur {
-                None=>ret.push(None),
-                Some(rf)=>{
-                    ret.push(Some(rf.borrow().val));
-                    if rf.borrow().left.is_none() && rf.borrow().right.is_none() && q.is_empty() {continue}
-                    q.push_back(rf.borrow().left.clone());
-                    q.push_back(rf.borrow().right.clone());
-                }
-            }
-        }
-        ret
-    }
-    fn from_vec(tree:Vec<Option<i32>>)-> Option<Rc<RefCell<TreeNode>>> {
-        fn construct(i: Option<i32>) -> Option<Rc<RefCell<TreeNode>>> {
-            match i {
-                Some(var)=>Some(Rc::new(RefCell::new(TreeNode{left:None, right:None, val:var}))),
-                None=>None
-            }
-        }
-        match tree.len() {
-            0=>None,
-            _=>{
-                let mut q = VecDeque::new();
-                let ret =construct(tree[0]);
-                q.push_back(ret.clone());
-                let mut idx = 1;
-                while !q.is_empty() {
-                    let cur = q.pop_front().unwrap();
-                    match cur {
-                        None=> continue,
-                        Some(rf)=>{
-                            if idx<tree.len() {
-                                let left = construct(tree[idx]);
-                                q.push_back(left.clone());
-                                rf.borrow_mut().left = left;
-                                idx+=1;
-                            }
-                            if idx<tree.len() {
-                                let right = construct(tree[idx]);
-                                q.push_back(right.clone());
-                                rf.borrow_mut().right = right;
-                                idx+=1;
-                            }
-                        }
-                    }
-                }
-                ret
-            }
-        }
-    }
+    use crate::Solution;
 
-    use std::cell::RefCell;
-    use std::rc::Rc;
-    use crate::{Solution, TreeNode};
     #[test]
     fn testcase_1() {
-        assert_eq!(2, Solution::pseudo_palindromic_paths(from_vec(vec![Some(2),Some(3),Some(1),Some(3),Some(1),None,Some(1)])))
+        assert_eq!(vec![vec![24,12],vec![8,6]], Solution::construct_product_matrix(vec![vec![1,2],vec![3,4]]));
     }
 
 
     #[test]
     fn testcase_2() {
-        assert_eq!(1, Solution::pseudo_palindromic_paths(from_vec(vec![Some(2),Some(1),Some(1),Some(1),Some(3),None,None,None,None,None,Some(1)])))
+        assert_eq!(vec![vec![2],vec![0],vec![0]], Solution::construct_product_matrix(vec![vec![12345],vec![2],vec![1]]));
     }
 
     #[test]
     fn testcase_3() {
-        assert_eq!(1, Solution::pseudo_palindromic_paths(from_vec(vec![Some(9)])))
-    }
-    #[test]
-    fn testcase_4() {
-        assert_eq!(1, Solution::pseudo_palindromic_paths(from_vec(vec![Some(9),Some(5),Some(4),Some(5),None,Some(2),Some(6),Some(2),Some(5),None,Some(8),Some(3),Some(9),Some(2),Some(3),Some(1),Some(1),None,Some(4),Some(5),Some(4),Some(2),Some(2),Some(6),Some(4),None,None,Some(1),Some(7),None,Some(5),Some(4),Some(7),None,None,Some(7),None,Some(1),Some(5),Some(6),Some(1),None,None,None,None,Some(9),Some(2),None,Some(9),Some(7),Some(2),Some(1),None,None,None,Some(6),None,None,None,None,None,None,None,None,None,Some(5),None,None,Some(3),None,None,None,Some(8),None,Some(1),None,None,Some(8),None,None,None,None,Some(2),None,Some(8),Some(7)])))
-    }
-    #[test]
-    fn test_helper() {
-        assert_eq!(vec![Some(2),Some(1),Some(1),Some(1),Some(3),None,None,None,None,None,Some(1)], to_vec(from_vec(vec![Some(2),Some(1),Some(1),Some(1),Some(3),None,None,None,None,None,Some(1)])));
+        assert_eq!(vec![vec![1462],vec![3103],vec![9436]], Solution::construct_product_matrix(vec![vec![414750857],vec![449145368],vec![767292749]]));
     }
 }
