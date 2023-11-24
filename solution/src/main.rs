6b52e4eab7dcb69739354b3c2e23478f17354963
Haoning <HaoningYe@outlook.com>
2023-11-24 15:47:26 -0500

    2023.11.24 Medium#1457 :bitwise:binaryTree:depthFirstSearch:

--- solution/src/main.rs
+++ solution/src/main.rs
@@ -1,21 +1,53 @@
 mod test;
 
 pub struct Solution;
-impl Solution {
-    pub fn max_array_value(nums: Vec<i32>) -> i64 {
-        let mut acc = 0_i64;
-        for n in (0..nums.len()).rev() {
-            let var = nums[n] as i64;
-            if acc >= var {
-                acc+=var;
+
+// Definition for a binary tree node.
+#[derive(Debug, PartialEq, Eq)]
+pub struct TreeNode {
+    pub val: i32,
+    pub left: Option<Rc<RefCell<TreeNode>>>,
+    pub right: Option<Rc<RefCell<TreeNode>>>,
+}
+
+impl TreeNode {
+    #[inline]
+    pub fn new(val: i32) -> Self {
+        TreeNode {
+            val,
+            left: None,
+            right: None,
+        }
+    }
+}
+
+use std::rc::Rc;
+use std::cell::RefCell;
+
+type T = Option<Rc<RefCell<TreeNode>>>;
+
+fn cnt(n:i32)-> i32 {
+    (1..=9).map(|x| if (n>>x)&1==1 {1} else {0}).sum()
+}
+fn dfs(root: T, acc: i32) -> i32 {
+    match root {
+        Some(rf) => {
+            let cur = acc ^ (1 << rf.borrow().val);
+            if rf.borrow().left.is_none() && rf.borrow().right.is_none() && cnt(cur) <= 1 {
+                1
             } else {
-                acc = var;
+                dfs(rf.borrow().left.clone(),cur) +dfs(rf.borrow().right.clone(), cur)
             }
         }
-        acc
+        None => 0
+    }
+}
+
+impl Solution {
+    pub fn pseudo_palindromic_paths(root: Option<Rc<RefCell<TreeNode>>>) -> i32 {
+        dfs(root, 0)
     }
 }
 
 
-fn main() {
-}
+fn main() {}
--- solution/src/test.rs
+++ solution/src/test.rs
@@ -1,25 +1,88 @@
 #[cfg(test)]
 mod test {
-    use crate::Solution;
+    use std::collections::VecDeque;
+    fn to_vec(tree:Option<Rc<RefCell<TreeNode>>>) -> Vec<Option<i32>> {
+        if tree.is_none() {return vec![]}
+        let mut ret = vec![];
+        let mut q = VecDeque::new();
+        q.push_back(tree);
+        while !q.is_empty() {
+            let cur = q.pop_front().unwrap();
+            match cur {
+                None=>ret.push(None),
+                Some(rf)=>{
+                    ret.push(Some(rf.borrow().val));
+                    if rf.borrow().left.is_none() && rf.borrow().right.is_none() && q.is_empty() {continue}
+                    q.push_back(rf.borrow().left.clone());
+                    q.push_back(rf.borrow().right.clone());
+                }
+            }
+        }
+        ret
+    }
+    fn from_vec(tree:Vec<Option<i32>>)-> Option<Rc<RefCell<TreeNode>>> {
+        fn construct(i: Option<i32>) -> Option<Rc<RefCell<TreeNode>>> {
+            match i {
+                Some(var)=>Some(Rc::new(RefCell::new(TreeNode{left:None, right:None, val:var}))),
+                None=>None
+            }
+        }
+        match tree.len() {
+            0=>None,
+            _=>{
+                let mut q = VecDeque::new();
+                let ret =construct(tree[0]);
+                q.push_back(ret.clone());
+                let mut idx = 1;
+                while !q.is_empty() {
+                    let cur = q.pop_front().unwrap();
+                    match cur {
+                        None=> continue,
+                        Some(rf)=>{
+                            if idx<tree.len() {
+                                let left = construct(tree[idx]);
+                                q.push_back(left.clone());
+                                rf.borrow_mut().left = left;
+                                idx+=1;
+                            }
+                            if idx<tree.len() {
+                                let right = construct(tree[idx]);
+                                q.push_back(right.clone());
+                                rf.borrow_mut().right = right;
+                                idx+=1;
+                            }
+                        }
+                    }
+                }
+                ret
+            }
+        }
+    }
+
+    use std::cell::RefCell;
+    use std::rc::Rc;
+    use crate::{Solution, TreeNode};
     #[test]
-// 输入：nums = [2,3,7,9,3]
-// 输出：21
     fn testcase_1() {
-        assert_eq!(21, Solution::max_array_value(vec![2, 3, 7, 9, 3]))
+        assert_eq!(2, Solution::pseudo_palindromic_paths(from_vec(vec![Some(2),Some(3),Some(1),Some(3),Some(1),None,Some(1)])))
     }
 
 
     #[test]
-// 输入：nums = [5,3,3]
-// 输出：11
     fn testcase_2() {
-        assert_eq!(11, Solution::max_array_value(vec![5, 3, 3]))
+        assert_eq!(1, Solution::pseudo_palindromic_paths(from_vec(vec![Some(2),Some(1),Some(1),Some(1),Some(3),None,None,None,None,None,Some(1)])))
     }
 
     #[test]
-// 输入：nums = [40,15,35,98,77,79,24,62,53,84,97,16,30,22,49]
-// 输出：781
     fn testcase_3() {
-        assert_eq!(781, Solution::max_array_value(vec![40, 15, 35, 98, 77, 79, 24, 62, 53, 84, 97, 16, 30, 22, 49]))
+        assert_eq!(1, Solution::pseudo_palindromic_paths(from_vec(vec![Some(9)])))
+    }
+    #[test]
+    fn testcase_4() {
+        assert_eq!(1, Solution::pseudo_palindromic_paths(from_vec(vec![Some(9),Some(5),Some(4),Some(5),None,Some(2),Some(6),Some(2),Some(5),None,Some(8),Some(3),Some(9),Some(2),Some(3),Some(1),Some(1),None,Some(4),Some(5),Some(4),Some(2),Some(2),Some(6),Some(4),None,None,Some(1),Some(7),None,Some(5),Some(4),Some(7),None,None,Some(7),None,Some(1),Some(5),Some(6),Some(1),None,None,None,None,Some(9),Some(2),None,Some(9),Some(7),Some(2),Some(1),None,None,None,Some(6),None,None,None,None,None,None,None,None,None,Some(5),None,None,Some(3),None,None,None,Some(8),None,Some(1),None,None,Some(8),None,None,None,None,Some(2),None,Some(8),Some(7)])))
+    }
+    #[test]
+    fn test_helper() {
+        assert_eq!(vec![Some(2),Some(1),Some(1),Some(1),Some(3),None,None,None,None,None,Some(1)], to_vec(from_vec(vec![Some(2),Some(1),Some(1),Some(1),Some(3),None,None,None,None,None,Some(1)])));
     }
 }
