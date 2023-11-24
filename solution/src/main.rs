1dc24f33d462a6e450614d474ab235a6df09f09e
Haoning <HaoningYe@outlook.com>
2023-11-24 11:49:41 -0500

    restructure test and crate use

--- solution/src/main.rs
+++ solution/src/main.rs
@@ -1,5 +1,6 @@
+mod test;
 
-struct Solution;
+pub struct Solution;
 impl Solution {
     pub fn max_array_value(nums: Vec<i32>) -> i64 {
         let mut acc = 0_i64;
@@ -15,28 +16,6 @@ impl Solution {
     }
 }
 
-#[test]
-// 输入：nums = [2,3,7,9,3]
-// 输出：21
-fn testcase_1() {
-    assert_eq!(21, Solution::max_array_value(vec![2,3,7,9,3]))
-}
-
-
-
-#[test]
-// 输入：nums = [5,3,3]
-// 输出：11
-fn testcase_2() {
-    assert_eq!(11, Solution::max_array_value(vec![5,3,3]))
-}
-
-#[test]
-// 输入：nums = [40,15,35,98,77,79,24,62,53,84,97,16,30,22,49]
-// 输出：781
-fn testcase_3() {
-    assert_eq!(781, Solution::max_array_value(vec![40,15,35,98,77,79,24,62,53,84,97,16,30,22,49]))
-}
 
 fn main() {
 }
--- solution/src/test.rs
+++ solution/src/test.rs
@@ -1,7 +1,25 @@
+#[cfg(test)]
+mod test {
+    use crate::Solution;
+    #[test]
+// 输入：nums = [2,3,7,9,3]
+// 输出：21
+    fn testcase_1() {
+        assert_eq!(21, Solution::max_array_value(vec![2, 3, 7, 9, 3]))
+    }
 
-#[test]
-// 输入：nums = [-1,1,2,3,1], target = 2
-// 输出：3
-fn testcase_1() {
-    assert_eq!(0,0)
+
+    #[test]
+// 输入：nums = [5,3,3]
+// 输出：11
+    fn testcase_2() {
+        assert_eq!(11, Solution::max_array_value(vec![5, 3, 3]))
+    }
+
+    #[test]
+// 输入：nums = [40,15,35,98,77,79,24,62,53,84,97,16,30,22,49]
+// 输出：781
+    fn testcase_3() {
+        assert_eq!(781, Solution::max_array_value(vec![40, 15, 35, 98, 77, 79, 24, 62, 53, 84, 97, 16, 30, 22, 49]))
+    }
 }
