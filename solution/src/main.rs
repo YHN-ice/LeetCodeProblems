4bd3b666b8e55fcc615992811ec522c08e140393
Haoning <HaoningYe@outlook.com>
2023-12-15 12:02:37 -0500

    Hard#1719 :recursive:tree:dfs:

--- solution/src/main.rs
+++ solution/src/main.rs
@@ -1,49 +1,83 @@
-mod test;
-
-pub struct Solution;
-
-trait Linear<T> {
-    fn lget(&self, i:usize) -> &T ;
-
-    fn lset(&mut self, i:usize)-> &mut T ;
-}
-impl Linear<i32> for Vec<Vec<i32>> {
-    fn lget(&self, i:usize) -> &i32 {
-        let n = self[0].len();
-        &self[i/n][i%n]
-    }
-    fn lset(&mut self, i:usize)-> &mut i32 {
-        let n = self[0].len();
-        &mut self[i/n][i%n]
-    }
-}
-
-const  MOD:i32 = 12345;
-
+// leetcode 1719
 impl Solution {
-    pub fn construct_product_matrix(grid: Vec<Vec<i32>>) -> Vec<Vec<i32>> {
-        let fold_n = |data,  n:usize| -> Vec<Vec<i32>> {
-            let mut ret = vec![];
-            let mut buf = vec![];
-            for d in data {
-                if buf.len()==n {ret.push(buf.clone()); buf.clear();}
-                buf.push(d);
-            }
-            if buf.len()>0 {ret.push(buf);}
-            ret
-        };
-        let nm = grid.len()*grid[0].len();
-        let (mut acc, mut racc) = (1, 1);
-        let pref = (0..nm).rev().map(|x| {acc = acc*(if x== nm-1 {1} else {grid.lget(x+1)%MOD})%MOD; acc}).collect::<Vec<_>>();
-        fold_n(
-            (0..nm).map(|i| {
-                let ret = (pref[nm-i-1] * racc) %MOD;
-                racc = racc * (grid.lget(i) %MOD) %MOD;
+    pub fn check_ways(pairs: Vec<Vec<i32>>) -> i32 {
+        use std::collections::{HashSet, HashMap};
+        // recursive credit to @DBabichev, https://leetcode.com/DBabichev/
 
-                ret
-            }), grid[0].len())
+        // 1. find the root, if can not find one return 0
+        // 2. remove the edges from root to others, form connected components for the rest of the graph
+        // 3. for each component, run check_ways recursively, and check the aggregate result as follows
+        //    1. if any components return 0, the overall result is 0
+        //    2. if all components return 1, then return 1
+        //    3. if some return 2 and others return 1, return 2
+        fn rec(graph: &mut HashMap<i32, HashSet<i32>>, mut sub_vertices: Vec<i32>)->i32 {
+            // first find the root, by collect the length of adjacent list, root should have its adj list of |V| - 1
+            let v = sub_vertices.len();
+            sub_vertices.sort_by_key(|node| graph.get(node).map(|x| x.len()).unwrap());
+
+            let root = sub_vertices[v-1]; // find the root
+            let mut candidate_root = if v >= 2 {sub_vertices.get(v-2)} else {None};
+            // eprintln!("root = {} \t graph.get(&root).map(|x| x.len()).unwrap_or_default() != v-1:{}, {}",root,graph.get(&root).map(|x| x.len()).unwrap_or_default() , v-1 );
+            if graph.get(&root).map(|x| x.len()).unwrap_or_default() != v-1 {return 0;} // if we can't, return 0
+            if let Some(cand) = candidate_root.take() {
+                if graph.get(&cand).map(|x| x.len()).unwrap_or_default() == v-1 {
+                    candidate_root = Some(cand); //resume
+                }
+            }
+            for nei in graph.get(&root).unwrap().clone() {graph.get_mut(&nei).map(|x| x.remove(&root));} // remove edges from root to others
+            let mut seen = HashSet::new();
+            fn dfs (node:i32, graph: &HashMap<i32, HashSet<i32>>, ret:&mut Vec<i32>, seen:&mut HashSet<i32>){
+                for nei in graph.get(&node).unwrap() {
+                    if !seen.contains(nei) {
+                        seen.insert(*nei);
+                        dfs(*nei, graph, ret, seen);
+                    }
+                }
+                ret.push(node)
+            }
+            let mut componentents = vec![];
+            for &node in &sub_vertices {
+                if node == root || seen.contains(&node) {continue}
+                seen.insert(node);
+                let mut ret = vec![];
+                dfs(node, graph, &mut ret, &mut seen);
+                componentents.push(ret);
+            }
+            let mut subres = vec![];
+            for component in componentents {
+                subres.push(rec(graph, component));
+            }
+            if subres.iter().any(|x| *x==0) {
+                0
+            } else if subres.iter().any(|x| *x==2) {
+                // eprintln!("mult comp");
+                2
+            } else if candidate_root.is_some() {
+                // eprintln!("mult root");
+                2
+            } else {
+                1
+            }
+        }
+        let mut graph:HashMap<i32, HashSet<i32>> = HashMap::new();
+        let mut all_nodes = vec![];
+        for pair in pairs {
+            let (a,b) = (pair[0], pair[1]);
+            graph.entry(a).and_modify(|x| {x.insert(b);}).or_insert_with(|| {
+                let mut ret = HashSet::new();
+                ret.insert(b); ret
+            });
+            graph.entry(b).and_modify(|x| {x.insert(a);}).or_insert_with(|| {
+                let mut ret = HashSet::new();
+                ret.insert(a); ret
+            });
+        }
+        for n in graph.keys() {
+            all_nodes.push(*n);
+        }
+        rec(&mut graph, all_nodes)
     }
 }
-
-
 fn main() {}
+mod test;
+struct Solution;
--- solution/src/test.rs
+++ solution/src/test.rs
@@ -3,18 +3,11 @@ mod test {
     use crate::Solution;
 
     #[test]
-    fn testcase_1() {
-        assert_eq!(vec![vec![24,12],vec![8,6]], Solution::construct_product_matrix(vec![vec![1,2],vec![3,4]]));
+    fn tc1 () {
+        assert_eq!(2,  Solution::check_ways(vec![vec![1,2],vec![2,3],vec![1,3]]))
     }
-
-
     #[test]
-    fn testcase_2() {
-        assert_eq!(vec![vec![2],vec![0],vec![0]], Solution::construct_product_matrix(vec![vec![12345],vec![2],vec![1]]));
-    }
-
-    #[test]
-    fn testcase_3() {
-        assert_eq!(vec![vec![1462],vec![3103],vec![9436]], Solution::construct_product_matrix(vec![vec![414750857],vec![449145368],vec![767292749]]));
+    fn tc2 () {
+        assert_eq!(1,  Solution::check_ways(vec![vec![1,2],vec![2,3]]))
     }
 }
