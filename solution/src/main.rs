b17eb105a2677e01ef819c35369d04c617ccd6cc
Haoning <HaoningYe@outlook.com>
2023-12-15 14:18:37 -0500

    Hard#1719 :bitop:setop:tree:dfs:

--- solution/src/main.rs
+++ solution/src/main.rs
@@ -1,81 +1,47 @@
+
 // leetcode 1719
 impl Solution {
     pub fn check_ways(pairs: Vec<Vec<i32>>) -> i32 {
-        use std::collections::{HashSet, HashMap};
-        // recursive credit to @DBabichev, https://leetcode.com/DBabichev/
+        // set relation
 
-        // 1. find the root, if can not find one return 0
-        // 2. remove the edges from root to others, form connected components for the rest of the graph
-        // 3. for each component, run check_ways recursively, and check the aggregate result as follows
-        //    1. if any components return 0, the overall result is 0
-        //    2. if all components return 1, then return 1
-        //    3. if some return 2 and others return 1, return 2
-        fn rec(graph: &mut HashMap<i32, HashSet<i32>>, mut sub_vertices: Vec<i32>)->i32 {
-            // first find the root, by collect the length of adjacent list, root should have its adj list of |V| - 1
-            let v = sub_vertices.len();
-            sub_vertices.sort_by_key(|node| graph.get(node).map(|x| x.len()).unwrap());
+        // for a node v denote its connected nodes as in pairs as S(v) = adj(v) \cup {v}
+        // for node u,v, if u MUST be ancestor of v, then S(v) is subset of S(u)
+        //               if u and v can be mutual ancestor/replaced, S(u) = S(v)
+        //               if u and v are not connected by pairs, i.e., in different subtrees, S(u) \notsubseteq S(v) \land S(v) \notsubseteq S(u)
+        // thus for a valid tree, we can construct a linear order for all distinct S(.), additionally we return 2 if
+        //      for any S(v) there are more than 1 corresponding v
+        // So if we can construct a tree from pairs, then each u,v in pair should satisfy
+        //      S(u) \subseteq S(v) \lor S(v) \subseteq S(u), \forall u,v in pairs
+        // furthur if both sides holds, S(u)=S(v), then we could return 2
+        // otherwise, conflict encountered and return 0
+        let mut bm = [[0_u128;4]; 500];
+        for pair in &pairs {
+            let (a,b) = (pair[0] as usize, pair[1] as usize);
+            bm[a][b / 128] |= 1<<(b % 128);
+            bm[b][a / 128] |= 1<<(a % 128);
 
-            let root = sub_vertices[v-1]; // find the root
-            let mut candidate_root = if v >= 2 {sub_vertices.get(v-2)} else {None};
-            // eprintln!("root = {} \t graph.get(&root).map(|x| x.len()).unwrap_or_default() != v-1:{}, {}",root,graph.get(&root).map(|x| x.len()).unwrap_or_default() , v-1 );
-            if graph.get(&root).map(|x| x.len()).unwrap_or_default() != v-1 {return 0;} // if we can't, return 0
-            if let Some(cand) = candidate_root.take() {
-                if graph.get(&cand).map(|x| x.len()).unwrap_or_default() == v-1 {
-                    candidate_root = Some(cand); //resume
-                }
+            bm[b][b / 128] |= 1<<(b % 128);
+            bm[a][a / 128] |= 1<<(a % 128);
+        }
+        let cnt_bit = |x:&[u128;4]| {x.iter().map(|y| y.count_ones()).sum()};
+        let v_cnt = bm.iter().filter(|x| cnt_bit(x) != 0).count() as u32;
+        if bm.iter().map(cnt_bit).max() != Some(v_cnt) {return 0}
+        let subseteq = |a:[u128;4], b:[u128;4]| {
+            let res = a.iter().zip(b.iter()).map(|(x,y)| x & y);
+            res.eq(a.into_iter())
+        };
+        let mut mult = false;
+        for pair in &pairs {
+            let (a,b) = (pair[0] as usize, pair[1] as usize);
+            if !(subseteq(bm[a],bm[b])||subseteq(bm[b],bm[a])) {
+                return 0;
             }
-            for nei in graph.get(&root).unwrap().clone() {graph.get_mut(&nei).map(|x| x.remove(&root));} // remove edges from root to others
-            let mut seen = HashSet::new();
-            fn dfs (node:i32, graph: &HashMap<i32, HashSet<i32>>, ret:&mut Vec<i32>, seen:&mut HashSet<i32>){
-                for nei in graph.get(&node).unwrap() {
-                    if !seen.contains(nei) {
-                        seen.insert(*nei);
-                        dfs(*nei, graph, ret, seen);
-                    }
-                }
-                ret.push(node)
-            }
-            let mut componentents = vec![];
-            for &node in &sub_vertices {
-                if node == root || seen.contains(&node) {continue}
-                seen.insert(node);
-                let mut ret = vec![];
-                dfs(node, graph, &mut ret, &mut seen);
-                componentents.push(ret);
-            }
-            let mut subres = vec![];
-            for component in componentents {
-                subres.push(rec(graph, component));
-            }
-            if subres.iter().any(|x| *x==0) {
-                0
-            } else if subres.iter().any(|x| *x==2) {
-                // eprintln!("mult comp");
-                2
-            } else if candidate_root.is_some() {
-                // eprintln!("mult root");
-                2
-            } else {
-                1
+            if subseteq(bm[a],bm[b])&&subseteq(bm[b],bm[a]) {
+                eprintln!("{:?}, {:?}", bm[a], bm[b]);
+                mult = true
             }
         }
-        let mut graph:HashMap<i32, HashSet<i32>> = HashMap::new();
-        let mut all_nodes = vec![];
-        for pair in pairs {
-            let (a,b) = (pair[0], pair[1]);
-            graph.entry(a).and_modify(|x| {x.insert(b);}).or_insert_with(|| {
-                let mut ret = HashSet::new();
-                ret.insert(b); ret
-            });
-            graph.entry(b).and_modify(|x| {x.insert(a);}).or_insert_with(|| {
-                let mut ret = HashSet::new();
-                ret.insert(a); ret
-            });
-        }
-        for n in graph.keys() {
-            all_nodes.push(*n);
-        }
-        rec(&mut graph, all_nodes)
+        if mult {2} else {1}
     }
 }
 fn main() {}
--- solution/src/test.rs
+++ solution/src/test.rs
@@ -10,4 +10,8 @@ mod test {
     fn tc2 () {
         assert_eq!(1,  Solution::check_ways(vec![vec![1,2],vec![2,3]]))
     }
+    #[test]
+    fn tc3 () {
+        assert_eq!(0,  Solution::check_ways(vec![vec![3,4],vec![2,3],vec![4,5],vec![2,4],vec![2,5],vec![1,5],vec![1,4]]))
+    }
 }
