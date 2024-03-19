419fc79e045810cc3140c0397b5a72e6150ab32a
Haoning <HaoningYe@outlook.com>
2024-03-19 15:52:48 -0400

    Hard:3086:greedy median:sliding-window:leetcode.com

--- solution/src/main.rs
+++ solution/src/main.rs
@@ -1,88 +1,110 @@
-#[derive(Debug)]
-struct Trie<> {
-    ptr: [usize; 26],
-    term: Option<String>
-}
-
-impl Trie {
-    fn new() -> Self {
-        Trie{ptr:[0;26], term:None}
-    }
-    fn build(dict:Vec<String>) -> (Vec<Trie>, usize) {
-        let mut storage = vec![Trie::new()];
-        let root = 0;
-        dict.into_iter().for_each(|x| {
-            let mut cur = root;
-            x.bytes().map(|x| (x-b'a') as usize).for_each(|ch| {
-                if storage[cur].ptr[ch] == 0 {
-                    storage[cur].ptr[ch] = storage.len();
-                    storage.push(Trie::new());
+impl Solution {
+    pub fn minimum_moves(nums: Vec<i32>, k: i32, max_changes: i32) -> i64 {
+        (|| {
+            let (k, max_changes):(i64, i64) = (k.try_into().ok()?, max_changes.try_into().ok()?);
+            let ones:Vec<i64> = nums.into_iter().enumerate().filter_map(|(idx, elem)| {
+                match elem {
+                    1=>Some(idx.try_into().ok()?),
+                    _=>None
                 }
-                cur = storage[cur].ptr[ch];
-            });
-            storage[cur].term = Some(x)
-        });
-        (storage,root)
+            }).collect();
+            let l_end = ones.len().try_into().ok()?;
+            let presum:Vec<_> = ones.iter().scan(0_i64, |state:&mut i64, v:&i64| {*state += *v; Some(*state)}).collect();
+            (0..=(3_i64).min(k).min(l_end)).map_while(|save_for_native|{
+                let range_len = k-max_changes.min(k-save_for_native);
+                match range_len {
+                    0=>Some((k - range_len) * 2),
+                    _ =>{
+                        (0..l_end).zip(range_len..).map_while(|(l,r)| {
+                            if r>l_end{
+                                None
+                            }
+                            else {
+                                let mid = l+((r-l)>>1);
+                                let r_usize:usize = r.try_into().ok()?;
+                                let l_usize:usize = l.try_into().ok()?;
+                                let mid_usize:usize = mid.try_into().ok()?;
+                                let distance_sum =
+                                    presum[r_usize-1_usize] - presum[mid_usize]
+                                    - (r-1-mid) * ones[mid_usize]
+                                    + (mid-l+1) * ones[mid_usize]
+                                    - (presum[mid_usize] - (match l {
+                                        0=>0,
+                                        _=>presum[l_usize-1]
+                                    }));
+                                Some(distance_sum + (k - range_len) * 2)
+                            }
+                        }).min()
+                    }
+                }
+            }).min()
+        })().unwrap()
     }
 }
 
-fn get_shape(board:&Vec<Vec<char>>) -> (usize, usize) {
-    (board.len(), board.first().map(|x| x.len()).unwrap_or(0))
+#[test]
+fn test1(){
+    assert_eq!(3, Solution::minimum_moves(vec![1,1,0,0,0,1,1,0,0,1], 3, 1))
 }
 
-fn backtrack(trie:&mut Vec<Trie>, root:usize, board:&Vec<Vec<char>>, pos:(usize,usize)) -> impl Iterator<Item = String> {
-    let (n,m) = get_shape(&board);
-    let mut vis = vec![vec![false;m];n];
-    let mut ret = vec![];
-    fn dfs(cur:usize, trie:&mut Vec<Trie>, board:&Vec<Vec<char>>, pos:(usize, usize), vis:&mut Vec<Vec<bool>>, ret:&mut Vec<String>) {
-        let (n,m) = get_shape(board);
-        let (x,y) = pos;
-        let cur_ch = (board[x][y] as u8 - b'a') as usize;
-        match trie[cur].ptr[cur_ch] {
-            0=>return,
-            id => {
-                // hit on id
-                vis[x][y] = true;
-                if let Some(str_ref) = trie[id].term.take() {ret.push(str_ref)}
+#[test]
+fn test2(){
+    assert_eq!(4, Solution::minimum_moves(vec![0,0,0,0], 2,3))
+}
 
-                if trie[id].ptr.iter().all(|&x| x == 0) {
-                    vis[x][y] = false;
-                    trie[cur].ptr[cur_ch] = 0;
-                    return
-                }
+#[test]
+fn test3(){
+    assert_eq!(4, Solution::minimum_moves(vec![1,0,1,0,1], 3, 0))
+}
 
-                let diff = [-1,0,1,0,-1];
-                diff.iter().zip((&diff[1..]).into_iter()).for_each(|(dx, dy)|{
-                    let (x,y) = (x as i32+dx, y as i32+dy);
-                    if !(0..n as i32).contains(&x) || !(0..m as i32).contains(&y) || vis[x as usize][y as usize]{ return }
-                    dfs(id, trie, board, (x as usize, y as usize), vis, ret)
-                });
-                vis[x][y] = false;
-            }
+
+#[test]
+fn test4(){
+    assert_eq!(0, Solution::minimum_moves(vec![0,1], 1, 0))
+}
+
+#[test]
+fn test5(){
+    assert_eq!(1, Solution::minimum_moves(vec![1,1], 2, 0))
+}
+
+
+#[test]
+fn test6(){
+    assert_eq!(0, Solution::minimum_moves(vec![1,1], 1, 2))
+}
+#[test]
+fn test7(){
+    assert_eq!(1, Solution::minimum_moves(vec![1,1,1], 2, 0))
+}
+struct Solution;
+// https://codeforces.com/blog/entry/96067
+mod cf_read {
+    pub mod input {
+        use std::io;
+        use std::str::FromStr;
+
+        #[allow(dead_code)]
+        pub fn read_line() -> String {
+            let mut buffer = String::new();
+            io::stdin()
+                .read_line(&mut buffer)
+                .expect("failed to read line");
+
+            buffer
+        }
+
+        #[allow(dead_code)]
+        pub fn read<T: FromStr>() -> Result<T, T::Err> {
+            read_line().trim().parse::<T>()
+        }
+
+        #[allow(dead_code)]
+        pub fn read_vec<T: FromStr>() -> Result<Vec<T>, T::Err> {
+            read_line()
+                .split_whitespace()
+                .map(|x| x.parse::<T>())
+                .collect()
         }
     }
-    dfs(root, trie, board, pos, &mut vis, &mut ret);
-    ret.into_iter()
-}
-impl Solution {
-    pub fn find_words(board: Vec<Vec<char>>, words: Vec<String>) -> Vec<String> {
-        let (mut trie, root) = Trie::build(words);
-        let (n,m) = get_shape(&board);
-        (0..n).flat_map(|x:usize| (0..m).map(move |y:usize| (x,y))).flat_map(|(x,y)| {
-            backtrack(&mut trie, root, &board, (x,y))
-        }).collect()
-    }
-}
-
-struct Solution;
-
-
-fn main() {
-
-    // let board = vec![vec!['o','a','a','n'],vec!['e','t','a','e'],vec!['i','h','k','r'],vec!['i','e','a','t']];
-    // let words = vec!["oath","pea","eat","rain"];
-    // assert_eq!(vec!["oath","eat"].into_iter().map(|x| x.to_owned()).collect::<Vec<String>>(), Solution::find_words(board, words.into_iter().map(|x| x.to_owned()).collect()));
-    let board = vec![vec!['a','b']];
-    let words = vec!["a", "b"];
-    assert_eq!(vec!["a","b"].into_iter().map(|x| x.to_owned()).collect::<Vec<String>>(), Solution::find_words(board, words.into_iter().map(|x| x.to_owned()).collect()));
 }
