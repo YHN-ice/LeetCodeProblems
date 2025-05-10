a90a5ffaf334c8a9f21c0ce6ba61d81631b6a963
noah <haoningye@outlook.com>
2025-05-10 20:22:26 +0000

    re-structure, TODO find the problem and replace xx

---
+++ solution/default.nix
@@ -0,0 +1,10 @@
+{
+  pkgs ? import <nixpkgs> {}
+}:
+let manifest = (pkgs.lib.importTOML ./Cargo.toml).package;
+in
+pkgs.rustPlatform.buildRustPackage rec {
+  inherit (manifest) name version;
+  cargoLock.lockFile = ./Cargo.lock;
+  src = pkgs.lib.cleanSource ./.;
+}
